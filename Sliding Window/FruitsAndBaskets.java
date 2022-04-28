09ed88d59417cf6d41159fb063a9c5ebbdf0c88d
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-04-28 11:17:28 +0530

    Create FruitsAndBaskets.java

---
+++ Sliding Window/FruitsAndBaskets.java
@@ -0,0 +1,30 @@
+/*
+	Time Complexity: O(N)
+	Space Complexity: O(1)
+*/
+import java.util.*;
+public class Solution {
+	public static int findMaxFruits(String str, int n) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		int maxFruits = Integer.MIN_VALUE;
+		int k = 2;
+		
+		int start = 0;
+		for(int end = 0; end < str.length(); end++) {
+			char ch = str.charAt(end);
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+			
+			while (map.size() > k) {
+				char leftChar = str.charAt(start);
+				map.put(leftChar, map.get(leftChar) - 1);
+				if (map.get(leftChar) == 0) {
+					map.remove(leftChar);
+				}
+				start++;
+			}
+			maxFruits = Math.max(maxFruits, end - start + 1);
+		}
+		
+		return maxFruits;
+	}
+}
