b0626687862e15e3eb2fa50f17ab927615677433
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-04-29 19:21:41 +0530

    Create RotateImage.java

---
+++ Arrays/RotateImage.java
@@ -0,0 +1,35 @@
+/*
+	Time Complexity: O(N * N) + O(N * N). First O(N * N) for transposing the matrix and the second one for reversing the matrix.
+	Space Complexity: O(1).
+	
+	Problem Link: https://leetcode.com/problems/rotate-image/
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+    
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+    
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
