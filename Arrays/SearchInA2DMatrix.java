8f8a63facbe2abacc6ea5f9510217110dfea9f01
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-02 11:09:05 +0530

    Update SearchInA2DMatrix.java

--- Arrays/SearchInA2DMatrix.java
+++ Arrays/SearchInA2DMatrix.java
@@ -1,4 +1,37 @@
 // Approach 1
+/*
+  Time complexity: O(log(M*N)) = O(logM) + O(logN)
+  Space complexity: O(1)
+  
+  Where, M is the #rows and N is the #cols
+  
+  Problem Link: https://leetcode.com/problems/search-a-2d-matrix/
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int lo = 0;
+        if(matrix.length == 0) return false;
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int hi = (n * m) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+
+// Approach 2
 
 /*
   Time complexity: O(MlogN)
