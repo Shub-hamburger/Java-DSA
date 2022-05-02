4aa01c75a1d0b05d2bb8c98a552f14c6fea0af44
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-02 17:57:25 +0530

    Create Pow(x, n).java

---
+++ Arrays/Pow(x, n).java
@@ -0,0 +1,18 @@
+/*
+  Time Complexity: O(logN)
+  Space Complexity: O(logN) -> Auxiliary stack space
+  
+  Problem Link: https://leetcode.com/problems/powx-n/
+*/
+class Solution {
+    public double myPow(double x, int n) {
+        // base case
+        if (n == 0)
+            return 1;
+        
+        if (n < 0) {
+            return 1/x * myPow(1/x, -(n + 1));
+        }
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+}
