6b825b4d4eeb2d3058406f449ff1a9ba9300b108
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-09 00:19:00 +0530

    Create RemoveNthNodeFromEndOfList.java

---
+++ Linked List/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,24 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(1)
+*/
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        ListNode slow = dummy, fast = dummy;
+        dummy.next = head;
+        
+        // Move fast pointer n + 1 nodes ahead of slow
+        for(int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+            
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+            
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
