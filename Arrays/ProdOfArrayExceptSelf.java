f2f86a97b6b1ba1c1f4c37b8f1669952bb0beda4
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-11 00:55:43 +0530

    Create ProdOfArrayExceptSelf.java

---
+++ Arrays/ProdOfArrayExceptSelf.java
@@ -0,0 +1,24 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(1)
+*/
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        
+        int prod = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] = prod;
+            prod *= nums[i];
+        }
+        
+        prod = 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= prod;
+            prod *= nums[i];
+        }
+        
+        return ans;
+    }
+}
