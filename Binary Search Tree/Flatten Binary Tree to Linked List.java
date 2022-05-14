1f36b5ea74f24ae8c8e152ea150cf8e27db6b5e4
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-14 18:35:42 +0530

    Create Flatten Binary Tree to Linked List.java

---
+++ Binary Search Tree/Flatten Binary Tree to Linked List.java
@@ -0,0 +1,47 @@
+/*
+	Time complexity: O(N)
+	Space complexity: O(N) -> Auxiliary Stack Space
+	
+	Problem Link: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+*/
+class Solution {
+    /*
+    // using global variable
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        // base case
+        if (root == null)
+            return;
+        
+        // hypothesis
+        flatten(root.right);
+        flatten(root.left);
+        
+        // hypothesis
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+    */
+    
+	// without using global variable
+    private TreeNode helper(TreeNode root, TreeNode prev) {
+        // base case
+        if(root == null) 
+            return prev;
+        
+        // hypothesis
+        prev = helper(root.right, prev);    
+        prev = helper(root.left, prev);
+        
+        // induction
+        root.right = prev;
+        root.left = null;
+        
+        return root;
+    }
+    
+    public void flatten(TreeNode root) {
+        helper(root, null);
+    }
+}
