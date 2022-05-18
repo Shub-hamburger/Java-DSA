9c4a3a9ce07ba178695f5557c7aed049f431380b
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-18 23:13:46 +0530

    Create MaximumProductSubarray.java

---
+++ Dynamic Programming/MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(1)
+*/
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int res = nums[0];
+        
+        for(int i = 1; i < nums.length; i++) {
+            int prevMaxProd = maxProd;
+            maxProd = Math.max(nums[i], Math.max(maxProd * nums[i], minProd * nums[i]));
+            minProd = Math.min(nums[i], Math.min(prevMaxProd * nums[i], minProd * nums[i]));
+            res = Math.max(res, maxProd);
+        }
+        
+        return res;
+    }
+}
