f099f97c2534c1c14998104a7f413d955d946911
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-20 00:18:20 +0530

    Create MaximalSquare.java

---
+++ Dynamic Programming/MaximalSquare.java
@@ -0,0 +1,24 @@
+/*
+    Time complexity: O(N * 2)
+    Space complexity: O(N * 2)
+	
+	Problem Link: https://leetcode.com/problems/maximal-square/
+*/
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int maxLength = 0;
+        int[][] dp = new int[rows + 1][cols + 1];
+        
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1]));
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                }
+            }
+        }
+        
+        return maxLength * maxLength;
+    }
+}
