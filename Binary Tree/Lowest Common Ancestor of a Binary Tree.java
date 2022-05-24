42d7d4297d8391a9608b25bb09396c4875ff743f
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-25 00:07:57 +0530

    Update Lowest Common Ancestor of a Binary Tree.java

--- Binary Tree/Lowest Common Ancestor of a Binary Tree.java
+++ Binary Tree/Lowest Common Ancestor of a Binary Tree.java
@@ -1,6 +1,8 @@
 /*
     Time complexity: O(N)
     Space complexity: O(N) -> Stack space
+    
+    Problem Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
