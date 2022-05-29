94a71c59ac2c55f192c469101332a91a02a8b015
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-05-29 13:09:07 +0530

    Create SameTree.java

---
+++ Binary Tree/SameTree.java
@@ -0,0 +1,22 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(N)
+*/
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // base case
+        if (p == null && q == null)
+            return true;
+        else if (p != null && q == null)
+            return false;
+        else if (p == null && q != null)
+            return false;
+        
+        // hypothesis
+        boolean isLeftSame = isSameTree(p.left, q.left);
+        boolean isRightSame = isSameTree(p.right, q.right);
+        
+        // induction
+        return p.val == q.val && isLeftSame && isRightSame;
+    }
+}
