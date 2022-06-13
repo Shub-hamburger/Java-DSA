42d6b9f0eaa60faaf0b4d2dd49dcd38663794331
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-06-14 00:03:19 +0530

    Create SquaresOfSortedArray.java

---
+++ Arrays/SquaresOfSortedArray.java
@@ -0,0 +1,23 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(N)
+*/
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int[] res = new int[nums.length];
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (Math.abs(nums[start]) > Math.abs(nums[end])) {
+                res[i] = nums[start] * nums[start];
+                start++;
+            } else {
+                res[i] = nums[end] * nums[end];
+                end--;
+            }
+        }
+        
+        return res;
+    }
+}
