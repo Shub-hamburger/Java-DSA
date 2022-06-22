ab33514f753e7a5aff3ab00e2479794ea9542258
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-06-22 13:27:48 +0530

    Create RotateArray.java

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,27 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(1)
+*/
+class Solution {
+    public void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        
+        // Reverse first and second parts respectively
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        
+        // Reverse the whole array
+        reverse(nums, 0, n - 1);
+    }
+}
