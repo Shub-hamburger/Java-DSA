9663668ff90145c156b571c2c6dd8250f6b36deb
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-06-23 21:25:21 +0530

    Create SubarraySumEqualsK.java

---
+++ Arrays/SubarraySumEqualsK.java
@@ -0,0 +1,35 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(N)
+    
+    Intuition
+    ---------
+    The value of currsum exceeds the desired sum by currsum – sum. If this value is removed from currsum then the
+    desired sum can be obtained. From the map, find the number of subarrays previously found having sum equal to
+    currsum-sum. 
+*/
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int subSum = 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        for(int num: nums) {
+            subSum += num;
+            int diff = subSum - k;
+            
+            if (map.containsKey(diff)) {
+                count += map.get(diff);
+            }
+            
+            if (map.containsKey(subSum))
+                map.put(subSum, map.get(subSum) + 1);
+            else
+                map.put(subSum, 1);
+        }
+        
+        return count;
+    }
+}
