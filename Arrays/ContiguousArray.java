26eeebdb9ad63db42a3530d106953c933b5d45ab
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-06-23 21:12:00 +0530

    Create ContiguousArray.java

---
+++ Arrays/ContiguousArray.java
@@ -0,0 +1,30 @@
+/*
+    Time complexity: O(N)
+    Space complexity: O(N)
+*/
+class Solution {
+    public int findMaxLength(int[] nums) {        
+        int maxLen = 0;
+        int count = 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+                
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            
+            if (num == 0)
+                count--;
+            else
+                count++;
+            
+            if (map.containsKey(count))
+                maxLen = Math.max(maxLen, i - map.get(count));
+            else
+                map.put(count, i);
+            
+        }
+        
+        return maxLen;
+    }
+}
