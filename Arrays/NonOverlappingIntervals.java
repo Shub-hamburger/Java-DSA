f649de1cd93c7dcaa2d1606fd6ea3dcc9d9c6628
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-07-13 11:56:39 +0530

    Create NonOverlappingIntervals.java

---
+++ Arrays/NonOverlappingIntervals.java
@@ -0,0 +1,26 @@
+/*
+    Time complexity: O(nlogn)
+    Space complexity: O(1)
+*/
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals.length == 0 || intervals == null)
+            return 0;
+        
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int count = 0;
+        
+        int[] prevInterval = intervals[0];
+        for(int i = 1; i < intervals.length; i++) {
+            int[] currInterval = intervals[i];
+            if (currInterval[0] < prevInterval[1]) {
+                count++;
+            } 
+            else {
+                prevInterval = currInterval;
+            }
+        }
+        
+        return count;
+    }
+}
