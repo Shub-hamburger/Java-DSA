0979b272954ff58e9d9a2b1f6924eec9507443de
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-07-24 11:30:17 +0530

    Create SearchInA2DMatrix.java

---
+++ Binary Search/SearchInA2DMatrix.java
@@ -0,0 +1,32 @@
+/*
+    Time complexity: O(logMN)
+    Space complexity: O(1)
+    
+    Where, M is the #rows and N is the #cols
+	
+	Problem Link: https://leetcode.com/problems/search-a-2d-matrix/submissions/
+*/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int lo = 0;
+        if(matrix.length == 0) return false;
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int hi = (n * m) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+}
