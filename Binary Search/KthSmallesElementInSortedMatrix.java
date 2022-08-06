81cee3b9b211280f378d565b8ccfc3379560f959
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-08-06 22:33:18 +0530

    Create KthSmallesElementInSortedMatrix.java

---
+++ Binary Search/KthSmallesElementInSortedMatrix.java
@@ -0,0 +1,29 @@
+/*
+    Time complexity: O(n * log(k)), where k is difference between maximum and minimum values in our matrix.
+    Space complexity: O(1)
+*/
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length; 
+        
+        int lo = matrix[0][0], hi = matrix[m - 1][n - 1] + 1;
+        
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            int count = 0,  j = n - 1;
+            for(int i = 0; i < m; i++) {
+                while(j >= 0 && matrix[i][j] > mid) j--;
+                count += (j + 1);
+            }
+            
+            if(count < k) 
+                lo = mid + 1;
+            else 
+                hi = mid;
+        }
+        
+        return lo;
+    }
+}
