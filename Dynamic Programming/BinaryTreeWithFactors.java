1f074262b1a81637289fd3ceb1aa0bf84478dfd3
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-08-09 23:57:49 +0530

    Update BinaryTreeWithFactors.java

--- Dynamic Programming/BinaryTreeWithFactors.java
+++ Dynamic Programming/BinaryTreeWithFactors.java
@@ -1,34 +1,33 @@
 /*
     Time Complexity: O(n ^ 2)
     Space Complexity: O(N)
+    
+    Problem Link: https://leetcode.com/problems/binary-trees-with-factors/submissions/
 */
 class Solution {
-    private int mod = (int) 1e7 + 9;
-    public int numFactoredBinaryTrees(int[] arr) {
-        int n = arr.length;
-        Arrays.sort(arr);
-        long[] dp = new long[n];
+    public int numFactoredBinaryTrees(int[] A) {
+        int MOD = 1_000_000_007;
+        int N = A.length;
+        Arrays.sort(A);
+        long[] dp = new long[N];
         Arrays.fill(dp, 1);
-        
-        HashMap<Integer, Integer> idxMap = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            idxMap.put(arr[i], i);
-        }
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if (arr[i] % arr[j] == 0) {
-                    int right = arr[i] / arr[j];
-                    if (idxMap.containsKey(right))
-                        dp[i] = (dp[i] + dp[j] * dp[idxMap.get(right)]) % mod;
+
+        Map<Integer, Integer> index = new HashMap();
+        for (int i = 0; i < N; ++i)
+            index.put(A[i], i);
+
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < i; ++j) {
+                if (A[i] % A[j] == 0) { // A[j] is left child
+                    int right = A[i] / A[j];
+                    if (index.containsKey(right)) {
+                        dp[i] = (dp[i] + dp[j] * dp[index.get(right)]) % MOD;
+                    }
                 }
             }
-        }
-        
+
         long ans = 0;
-        for(long num: dp)
-            ans += num;
-        
-        return (int) (ans % mod);
+        for (long x: dp) ans += x;
+        return (int) (ans % MOD);
     }
 }
