6a3e65b1e2c4326315f73a72f0b8866115f7e548
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-09-12 23:01:32 +0530

    Create BagOfTokens.java

---
+++ Greedy/BagOfTokens.java
@@ -0,0 +1,32 @@
+/*
+    Time complexity: O(nlogn)
+    Space complexity: O(logn) -> taken by Arrays.sort()
+*/
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int left = 0;
+        int right = tokens.length - 1;
+        int score = 0;
+        int ans = 0;
+        
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                score++;
+                power -= tokens[left];
+                left++;
+                ans = Math.max(ans, score);
+            } 
+            else if (power < tokens[left] && score > 0) {
+                score -= 1;
+                power += tokens[right];
+                right -= 1;
+            }
+            else {
+                return ans;
+            }
+        }
+        
+        return ans;
+    }
+}
