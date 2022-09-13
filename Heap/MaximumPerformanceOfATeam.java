2dafa824da39efbf079a43cb7a4493b3e84435cb
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2022-09-13 23:43:19 +0530

    Create MaximumPerformanceOfATeam.java

---
+++ Heap/MaximumPerformanceOfATeam.java
@@ -0,0 +1,26 @@
+/*
+    Time complexity: O(NlogN)
+    Space complexity: O(N)
+*/
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] mat = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            mat[i] = new int[] {efficiency[i], speed[i]};
+        }
+        
+        Arrays.sort(mat, (a, b) -> b[0] - a[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
+        long ans = 0, sum = 0;
+        
+        for(int[] arr: mat) {
+            pq.add(arr[1]);
+            sum += arr[1];
+            if (pq.size() > k)
+                sum -= pq.poll();
+            ans = Math.max(ans, sum * arr[0]);
+        }
+        
+        return (int) (ans % (long)(1e9 + 7));
+    }
+}
